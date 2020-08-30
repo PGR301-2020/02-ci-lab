934929625965fb39a79489f7642db772da2674e9
glennbech <glenn.bech@gmail.com>
2020-08-30 16:40:19 +0200

    Initial commit

---
+++ bowlinggamekata/src/test/java/GameTest.java
@@ -0,0 +1,15 @@
+import no.pgr301.bowlinggame.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+
+    @Test
+    public void testNoStrikeNoSpare() {
+        Game game = new Game();
+        game.roll(5);
+        game.roll(4);
+        assertEquals(9, game.score());
+    }
+}
